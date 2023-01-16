d7024870a933d14bead17ec4718c25b87768b353
mercedes-arnwine <80288739+mercedes-arnwine@users.noreply.github.com>
2023-01-15 21:10:56 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fa-facebook");
+facebook.style.color = "#6a99b5";
+
+var openingHours = document.querySelector(".contact p");
+openingHours.style.border = "1px solid #d45bb9";
+
+openingHours.style.padding = "1em";
